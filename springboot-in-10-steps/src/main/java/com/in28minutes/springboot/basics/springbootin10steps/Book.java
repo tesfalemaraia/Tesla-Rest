057fed41e09a2a93b44774860ad43e64c156840e
Tesfalem Araia <tesfalemaraia@gmail.com>
2019-03-30 21:58:40 -0500

    Springboot-in-10-steps from in28minutes from Udemy.

---
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/Book.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+public class Book {
+	
+	long id;
+	String name;
+	String author;
+	
+	public Book(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+	
+	
+
+}
